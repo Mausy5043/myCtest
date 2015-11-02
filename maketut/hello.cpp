3fae4b9deeefc50b6a2711778525c104219d4641
Mausy5043 <Mausy5043@users.noreply.github.com>
2015-11-02 18:50:31 +0100

    20151102T1850

--- maketut/hello.cpp
+++ maketut/hello.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "functions.h"
+using namespace std;
 
 void print_hello(){
-   std::cout << "Hello World!";
+  cout << "Hello World!";
 }
--- maketut/main.cpp
+++ maketut/main.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 #include "functions.h"
+using namespace std;
 
 int main(){
-    print_hello();
-    std::cout << std::endl;
-    std::cout << "The factorial of 5 is " << factorial(5) << std::endl;
-    return 0;
+  print_hello();
+  cout << endl;
+  cout << "The factorial of 5 is " << factorial(5) << endl;
+  return 0;
 }
