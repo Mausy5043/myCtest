ce85732ccb53f711795a1b4a87ce8b83cc3d769c
Mausy5043 <Mausy5043@users.noreply.github.com>
2015-11-03 15:15:25 +0100

    20151103T1515

--- 00-update.sh
+++ 00-update.sh
@@ -8,6 +8,6 @@
  git fetch origin
  git checkout master
  git reset --hard origin/master && \
- git clean -f -d
+ #git clean -f -d
 
 chmod -R 744 *
--- fr/fr.cpp
+++ fr/fr.cpp
@@ -6,19 +6,13 @@ int write2file() {
   // declarations
   streambuf *psbuf, *backup;
   ofstream filestr;
-
   backup = cout.rdbuf();          // back up cout's streambuf
-
-  filestr.open ("test.txt");
-  psbuf = filestr.rdbuf();        // get file's streambuf
-  cout.rdbuf(psbuf);              // assign streambuf to cout
-
-  cout << "This is written to the file\n";
-
-  cout.rdbuf(backup);             // reassign cout's original streambuf
-
+  filestr.open("test.txt");
+    psbuf = filestr.rdbuf();        // get file's streambuf
+    cout.rdbuf(psbuf);              // assign streambuf to cout
+      cout << "This is written to the file\n";
+    cout.rdbuf(backup);             // reassign cout's original streambuf
   filestr.close();
-
   return 0;
 }
 
@@ -26,8 +20,7 @@ int invoer(){
   int sum = 0, value = 0;
   // read until end-of-file, calculating a running total of all values read
   cout << "Enter numbers to add. (^D to end input)" << endl;
-  while (cin >> value)
-  {
+  while (cin >> value){
     sum += value; // equivalent to sum = sum + value
   }
   cout << "Sum is: " << sum << endl;
