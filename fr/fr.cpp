f26f906c8823c9f85b9616fc0caf26a8a440bdef
Mausy5043 <Mausy5043@users.noreply.github.com>
2015-11-03 14:03:20 +0100

    print

--- fr/fr.cpp
+++ fr/fr.cpp
@@ -1,5 +1,27 @@
 #include <iostream>
+#include <fstream>
 using namespace std;
+
+int write2file() {
+  streambuf *psbuf, *backup;
+  ofstream filestr;
+  filestr.open ("test.txt");
+
+  backup = cout.rdbuf();          // back up cout's streambuf
+
+  psbuf = filestr.rdbuf();        // get file's streambuf
+  cout.rdbuf(psbuf);              // assign streambuf to cout
+
+  cout << "This is written to the file";
+
+  cout.rdbuf(backup);             // restore cout's original streambuf
+
+  filestr.close();
+
+  return 0;
+}
+
+
 
 int main(){
   cout << "This text is sent to `cout`." << endl;
