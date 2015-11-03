a157e4ddc96e7584e0a811dd9bc964777f1c9fd4
Mausy5043 <Mausy5043@users.noreply.github.com>
2015-11-03 14:22:59 +0100

    added code

--- 00-update.sh
+++ 00-update.sh
@@ -1,9 +1,12 @@
 #! /bin/bash
 
+ git fetch origin
+ git log --graph --oneline --date-order --decorate --color --all
+
  git pull
  git fetch origin
  git checkout master
  git reset --hard origin/master && \
- git clean -f -d
+ #git clean -f -d
 
 chmod -R 744 *
