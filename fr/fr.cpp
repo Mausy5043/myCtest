61e97ec661cca49145cd178048b445f5e8ffd0ee
Mausy5043 <Mausy5043@users.noreply.github.com>
2015-11-03 13:42:09 +0100

    new prog

---
+++ fr/fr.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  cout << "This text is sent to `cout`." << endl;
+  clog << "This text is sent to `clog`." << endl;
+  cerr << "This text is sent to `cerr`." << endl;
+  return 0;
+}
--- maketut/hello.cpp
+++ maketut/hello.cpp
@@ -2,6 +2,9 @@
 #include "functions.h"
 using namespace std;
 
-void print_hello(){
-  cout << "Hello World!";
+int main(){
+  print_hello();
+  cout << endl;
+  cout << "The factorial of 5 is " << factorial(5) << endl;
+  return 0;
 }
