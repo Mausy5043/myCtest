9485efeeedf4de149264dec201c480590dd84425
Mausy5043 <Mausy5043@users.noreply.github.com>
2015-11-04 13:11:38 +0100

    add fc

---
+++ fc/fc.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+  int r = 0;
+   printf("Hello world!\n");
+   /*
+   "Size of char : " << sizeof(char) << endl;
+   cout << "Size of int : " << sizeof(int) << endl;
+   cout << "Size of short int : " << sizeof(short int) << endl;
+   cout << "Size of long int : " << sizeof(long int) << endl;
+   cout << "Size of float : " << sizeof(float) << endl;
+   cout << "Size of double : " << sizeof(double) << endl;
+   cout << "Size of wchar_t : " << sizeof(wchar_t) << endl;
+  r = write2file();
+  r = invoer();
+  */
+  return r;
+}
