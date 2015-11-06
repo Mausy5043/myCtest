e3a63f548e1e718d9f7d6acd6d67c2b3db9928e9
Mausy5043 <Mausy5043@users.noreply.github.com>
2015-11-06 16:32:58 +0100

    20151106T1632

--- types/types.c
+++ types/types.c
@@ -3,23 +3,22 @@
 
 int main()
 {
-  printf("char\n");
-  printf("sizeof(char)        == %lu\n", sizeof(char));
-  printf("\nvarious\n");
-  printf("\nint\n");
-  printf("sizeof(int)         == %lu\n", sizeof(int));
-  printf("sizeof(short)       == %lu\n", sizeof(short));
-  printf("sizeof(short int)   == %lu\n", sizeof(short int));
-  printf("sizeof(long)        == %lu\n", sizeof(long));
-  printf("sizeof(long int)    == %lu\n", sizeof(long int));
-  printf("sizeof(long long)   == %lu\n", sizeof(long long));
-  printf("\ndouble\n");
-  printf("sizeof(double)      == %lu\n", sizeof(double));
+  printf("\tCHAR\n");
+  printf("sizeof(char)        == %i\n", (int)sizeof(char));
+  printf("\n\tINT\n");
+  printf("sizeof(int)         == %i\n", (int)sizeof(int));
+  printf("sizeof(short)       == %i\n", (int)sizeof(short));
+  printf("sizeof(short int)   == %i\n", (int)sizeof(short int));
+  printf("sizeof(long)        == %i\n", (int)sizeof(long));
+  printf("sizeof(long int)    == %i\n", (int)sizeof(long int));
+  printf("sizeof(long long)   == %i\n", (int)sizeof(long long));
+  printf("\n\tDOUBLE\n");
+  printf("sizeof(double)      == %i\n", (int)sizeof(double));
   //printf("double maximum      == %f\n", DBL_MAX);
-  printf("sizeof(long double) == %lu\n", sizeof(long double));
+  printf("sizeof(long double) == %i\n", (int)sizeof(long double));
   //printf("long double maximum == %Lf\n", LDBL_MAX);
-  printf("\nfloat\n");
-  printf("sizeof(float)       == %lu\n", sizeof(float));
+  printf("\n\tFLOAT\n");
+  printf("sizeof(float)       == %i\n", (int)sizeof(float));
   //printf("float maximum       == %f\n", FLT_MAX);
   //printf("sizeof(long float)  == %lu\n", sizeof(long float));
 }
