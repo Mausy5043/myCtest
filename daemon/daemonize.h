bf6564977371f53d9ec8d32ca58fa07aef1d66df
Mausy5043 <Mausy5043@users.noreply.github.com>
2015-11-07 13:37:17 +0100

    daemon added

---
+++ daemon/daemonize.h
@@ -0,0 +1,7 @@
+#ifndef DAEMONIZE_H
+#define DAEMONIZE_H
+
+// function declarations
+static void skeleton_daemon(void)
+
+#endif  /* DAEMONIZE_H */
---
+++ daemon/main.c
@@ -0,0 +1,19 @@
+# include "daemonize.h"
+
+int main()
+{
+    skeleton_daemon();
+
+    while (1)
+    {
+        //TODO: Insert daemon code here.
+        syslog (LOG_NOTICE, "First daemon started.");
+        sleep (20);
+        break;
+    }
+
+    syslog (LOG_NOTICE, "First daemon terminated.");
+    closelog();
+
+    return EXIT_SUCCESS;
+}
