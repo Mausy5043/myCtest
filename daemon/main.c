2515f61d2f3062063ad275b2f24276b9a65a584f
Mausy5043 <Mausy5043@users.noreply.github.com>
2015-11-07 14:20:36 +0100

    debugd

--- daemon/main.c
+++ daemon/main.c
@@ -10,9 +10,11 @@ To test signal:	kill -HUP `cat /tmp/exampled.lock`
 To terminate:	kill `cat /tmp/exampled.lock`
 */
 
-#include <stdio.h>
 #include <fcntl.h>
 #include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 
 #define RUNNING_DIR	"/tmp"
@@ -48,34 +50,34 @@ void daemonize()
 {
 int i,lfp;
 char str[10];
-  if(getppid()==1) return; /* already a daemon */
+  if(getppid()==1) return;                        /* already a daemon */
   i=fork();
-  if (i<0) exit(1); /* fork error */
-  if (i>0) exit(0); /* parent exits */
+  if (i<0) exit(1);                               /* fork error */
+  if (i>0) exit(0);                               /* parent exits */
   /* child (daemon) continues */
-  setsid(); /* obtain a new process group */
-  for (i=getdtablesize();i>=0;--i) close(i); /* close all descriptors */
-  i=open("/dev/null",O_RDWR); dup(i); dup(i); /* handle standart I/O */
-  umask(027); /* set newly created file permissions */
-  chdir(RUNNING_DIR); /* change running directory */
+  setsid();                                       /* obtain a new process group */
+  for (i=getdtablesize();i>=0;--i) close(i);      /* close all descriptors */
+  i=open("/dev/null",O_RDWR); dup(i); dup(i);     /* handle standart I/O */
+  umask(027);                                     /* set newly created file permissions */
+  chdir(RUNNING_DIR);                             /* change running directory */
   lfp=open(LOCK_FILE,O_RDWR|O_CREAT,0640);
-  if (lfp<0) exit(1); /* can not open */
-  if (lockf(lfp,F_TLOCK,0)<0) exit(0); /* can not lock */
+  if (lfp<0) exit(1);                             /* can not open */
+  if (lockf(lfp,F_TLOCK,0)<0) exit(0);            /* can not lock */
   /* first instance continues */
   sprintf(str,"%d\n",getpid());
-  write(lfp,str,strlen(str)); /* record pid to lockfile */
-  signal(SIGCHLD,SIG_IGN); /* ignore child */
-  signal(SIGTSTP,SIG_IGN); /* ignore tty signals */
+  write(lfp,str,strlen(str));                     /* record pid to lockfile */
+  signal(SIGCHLD,SIG_IGN);                        /* ignore child */
+  signal(SIGTSTP,SIG_IGN);                        /* ignore tty signals */
   signal(SIGTTOU,SIG_IGN);
   signal(SIGTTIN,SIG_IGN);
-  signal(SIGHUP,signal_handler); /* catch hangup signal */
-  signal(SIGTERM,signal_handler); /* catch kill signal */
+  signal(SIGHUP,signal_handler);                  /* catch hangup signal */
+  signal(SIGTERM,signal_handler);                 /* catch kill signal */
 }
 
 main()
 {
   daemonize();
-  while(1) sleep(1); /* run */
+  while(1) sleep(1);                              /* run */
 }
 
 /* EOF */
