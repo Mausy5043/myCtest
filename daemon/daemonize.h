ba6436af689a0ee76d0785707f632146bfd1b2d4
Mausy5043 <Mausy5043@users.noreply.github.com>
2015-11-07 14:47:55 +0100

    separate

--- daemon/daemonize.h
+++ daemon/daemonize.h
@@ -2,6 +2,7 @@
 #define DAEMONIZE_H
 
 // function declarations
-static void skeleton_daemon(void);
-
+void log_message(char *filename,char *message)
+void signal_handler(int sig)
+void daemonize(void)
 #endif  /* DAEMONIZE_H */
